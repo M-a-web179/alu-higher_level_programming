8a563fdbd3b2e95ea768f405c3bf0c35749a9698
M-a-web179 <m.ainomugis@alustudents.com>
2024-11-27 19:08:57 +0000

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all `states` with a name starting
-with `N` from the database `hbtn_0e_0_usa`.
+Script that lists all `states` from the database `hbtn_0e_0_usa`.
 Arguments:
     mysql username (str)
     mysql password (str)
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
 
     for row in rows:
